4333216805156525de1de021a7dbd3fc02697d3e
Webbie <r14941110@gmail.com>
2025-11-26 13:55:11 +0800

    mod trainscript

--- TrainScript.sh
+++ TrainScript.sh
@@ -6,4 +6,4 @@
 
 python ./train.py --tf_log --label_nc 0 --no_instance --netG local\
         --name OneTissueTask_localenhancer --batchSize 1 --loadSize 1024 --fineSize 1024 \
-        --dataroot /Workspace/Train_5x_20x_NoMBB_mix_19_20/ 
+        --dataroot /home/Webbie/pix2pix/datasets/Train_5x_20x_NoMBB_20_N/ 
